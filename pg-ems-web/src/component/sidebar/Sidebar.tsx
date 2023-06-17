732d3a32922e90e0e13cb3d611027750c7d7d715
WeiduoS <ws1255@nyu.edu>
2023-06-17 02:43:19 -0400

    Update basic main page

--- pg-ems-web/src/component/sidebar/Sidebar.tsx
+++ pg-ems-web/src/component/sidebar/Sidebar.tsx
@@ -1,23 +1,67 @@
 import React from "react";
 import './Sidebar.scss'
-import { AiFillProject, AiOutlineDashboard, AiOutlineForm } from 'react-icons/ai';
+import { AiFillProject, AiOutlineArrowLeft, AiOutlineDashboard, AiOutlineForm } from 'react-icons/ai';
 import { GoPerson } from 'react-icons/go';
-import { BsTable } from 'react-icons/bs';
+import { BsTable, BsTypeH1 } from 'react-icons/bs';
 import { RiFeedbackFill } from 'react-icons/ri';
 
 function Sidebar() {
 
+    let sidebarToggleStatus = false;
+
+    const toggleSidebar = () => {
+        if(sidebarToggleStatus) {
+            document.getElementsByClassName('sidebar-brand-text')[0]
+                .classList.remove('hidden')
+            Array.from(document.querySelectorAll('div.sidebar-nav-button'))
+                .forEach(e => e.classList.remove('hidden'));
+        }else {
+            document.getElementsByClassName('sidebar-brand-text')[0]
+                .classList.add('hidden')
+            Array.from(document.querySelectorAll('div.sidebar-nav-button'))
+                .forEach(e => e.classList.add('hidden'));
+
+        }
+        sidebarToggleStatus = !sidebarToggleStatus;
+    }
 
     return (
-        <div className={'sidebar-container'}>
-            <div className={'brand'}>EMP</div>
-            <div className={'sidebar-nav'}><AiOutlineDashboard className={'sidebar-nav-icon'}/> <div className={'sidebar-nav-button'}>Dashboard</div></div>
-            <div className={'sidebar-nav'}><BsTable className={'sidebar-nav-icon'}/><div className={'sidebar-nav-button'}>Attendance</div></div>
-            <div className={'sidebar-nav'}><AiFillProject className={'sidebar-nav-icon'}/><div className={'sidebar-nav-button'}>Projects</div></div>
-            <div className={'sidebar-nav'}><GoPerson className={'sidebar-nav-icon'}/><div className={'sidebar-nav-button'}>Account</div></div>
-            <div className={'sidebar-nav'}><AiOutlineForm className={'sidebar-nav-icon'}/><div className={'sidebar-nav-button'}>Request</div></div>
-            <div className={'sidebar-nav'}><RiFeedbackFill className={'sidebar-nav-icon'}/><div className={'sidebar-nav-button'}>Feedback</div></div>
-        </div>
+        <>
+            <aside className={'sidebar-container'}>
+                <AiOutlineArrowLeft
+                    className={'sidebar-toggle-button add-color'}
+                    onClick={() => toggleSidebar()}
+                />
+                <div className={'sidebar-brand'}>
+                    <BsTypeH1 className={'sidebar-nav-icon'} />
+                    <div className={'sidebar-brand-text'}>EMP</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <AiOutlineDashboard className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Dashboard</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <BsTable className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Attendance</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <AiFillProject className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Projects</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <GoPerson className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Account</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <AiOutlineForm className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Request</div>
+                </div>
+                <div className={'sidebar-nav'}>
+                    <RiFeedbackFill className={'sidebar-nav-icon'}/>
+                    <div className={'sidebar-nav-button'}>Feedback</div>
+                </div>
+            </aside>
+        </>
     );
 }
 
